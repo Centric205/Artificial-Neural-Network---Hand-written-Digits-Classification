2655b779abe1e89935018573e762de45401552ff
Theo Madikgetla <theocentric95@gmail.com>
2022-05-02 21:40:48 +0200

    Imported all the libraries that will be used in classifier_1

---
+++ classifier_1.py
@@ -0,0 +1,12 @@
+# Machine Learning: Artificial Neural Network (ANN)
+# Date: 17 April 2022
+# Developer: Theo Madikgetla
+
+import numpy as np
+import torch
+import torchvision.transforms.fnctional as TF
+import matplotlib.pyplot as plt
+from PIL import Image
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
