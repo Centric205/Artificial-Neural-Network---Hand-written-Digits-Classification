0fd592f9599f2f597a8d4cca39419a9fee196142
Theo Madikgetla <theocentric95@gmail.com>
2022-05-02 23:57:47 +0200

    Added code that determines log probabilities and outputs them

--- classifier_1.py
+++ classifier_1.py
@@ -135,18 +135,36 @@ for eps in range(epochs):
         print("Epoch {} - Training loss: {}". format(eps, running_loss/len(training_loader)))
 print("\nTraining Time (in minutes) = ", (time() - time_)/60)
 
+def classify(img, ps):
+    '''
+    Function for viewing an image and it's predicated classes. They will be shown to you.
 
+    '''
+    ps = ps.data.numpy().squeeze()
 
+    fig, (ax1, ax2) = plt.subplots(figsize=(6, 9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    ax2.set_yticklabels(np.arange(10))
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+    plt.tight_layout()
+    plt.show()
 
+images, labels = next(iter(testloader))
+# replace training_loader to check training accuracy.
 
+img = images[0].view(1, 784)
 
+# Turns off gradients to speed up this part
+with torch.no_grad():
+    log_prob = model(img)
 
-
-
-
-
-
-
-
-
-
+# Output of the network are log_probabilities, need to take exponential for probabilities
+pb = torch.exp(log_prob)
+probab = list(pb.numpy()[0])
+print("Predicted Digit =", probab.index(max(probab)))
+classify(img.view(1, 28, 28), pb)
