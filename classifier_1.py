b0bd92beca15b797845896817cad0e314267681a
Theo Madikgetla <theocentric95@gmail.com>
2022-05-02 22:54:39 +0200

    Created a ann model with three layers using sequential module

--- classifier_1.py
+++ classifier_1.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 import torch
-import torchvision.transforms.fnctional as TF
+import torchvision.transforms.functional as TF
 import matplotlib.pyplot as plt
 from PIL import Image
 from time import time
@@ -13,9 +13,9 @@ from torch import nn, optim
 
 # Sets makes the sample image sizes to be fixed.
 
-transform = transform.Compose(  [transforms.ToTensor(),
-                                 transforms.Normalize((0.5,), (0.5,)),
-                                ])
+transform = transforms.Compose([transforms.ToTensor(),
+                               transforms.Normalize((0.5,), (0.5,)),
+                              ])
 
 # Creates the training set and test set.
 
@@ -23,7 +23,7 @@ training_set = datasets.MNIST(root='input', download=False, train=True, transfor
 training_loader = torch.utils.data.DataLoader(training_set, batch_size=64, shuffle=True)
 
 testset = datasets.MNIST(root='input', download=False, train=False, transform=transform)
-testloader = torch.utils.data.Dataloader(testset, batch_size=64, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
 dataiter = iter(training_loader)
 images, labels = dataiter.next()
@@ -45,6 +45,26 @@ for index in range(1, num_of_images + 1):
     plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
 
 plt.show()
+
+# Creating Neural Network model using the sequential model
+# Three layer with different activation functions
+
+model = nn.Sequential(
+                        nn.Linear(784, 128),    # 1st layer
+                        nn.ReLU(),              # Activation function: Rectified Linear Function
+
+                        nn.Linear(128, 64),     # 2nd Layer
+                        nn.Tanh(),              # Activation Function: Hyperbolic Tangent Function
+
+                        nn.Linear(64, 10),      # 3rd Layer
+                        nn.LogSoftmax(dim=1)    # Activation Function: Log Softmax
+                     )
+
+print(model)
+
+
+
+
 
 
 
